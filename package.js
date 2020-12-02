8586c705aae34639a7aba0dae8a8fe2f07f2763c
soskarsson <siggi@vatfree.com>
2020-12-02 14:57:46 +0100

    Updated versions, linked to vatfree:i18n package

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
-  name: "tap:i18n-bundler",
-  summary: "Bundles your project's tap-i18n files and pre-loads them on the client automatically",
+  name: "vatfree:i18n-bundler",
+  summary: "Bundles your project's vatfree-i18n files and pre-loads them on the client automatically",
   version: '0.3.0',
-  git: 'https://github.com/TAPevents/tap-bundler'
+  git: 'https://github.com/vatfree/tap-bundler'
 });
 
 Package.on_use(function(api) {
-  api.versionsFrom('1.1.0.3');
+  api.versionsFrom('1.6');
 
   api.use([
-    'coffeescript',
+    'coffeescript@1.0.11',
     'jquery',
-    'tap:i18n@1.7.0'
+    'vatfree:i18n@1.8.1'
   ], ['client', 'server']);
 
   api.add_files("i18n-bundler.coffee", "server");
